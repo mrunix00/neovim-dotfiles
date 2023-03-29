f53bf3675adbebc87797e544924133d60b5769fc
Mr.UNIX <mrunix00@protonmail.com>
2023-03-29 17:59:18 +0100

    Initial commit

---
+++ .config/nvim/lua/lol/init.lua
@@ -0,0 +1,4 @@
+require("lol.keymaps")
+require("lol.set")
+require("lol.packer")
+require('lualine').setup()
---
+++ .config/nvim/lua/lol/keymaps.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Neotree)
+vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm)
